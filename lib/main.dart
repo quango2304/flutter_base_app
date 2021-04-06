5adaa87d800fc09b7426bedff0eead2f631aea73
Leo <quanngovan98@gmail.com>
2021-04-06 16:21:20 +0700

    improve

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,6 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_standard_app/blocs/auth/auth_cubit.dart';
-import 'package:flutter_standard_app/repositories/auth_repository.dart';
-import 'package:flutter_standard_app/repositories/user_repository.dart';
-import 'package:flutter_standard_app/screens/home_screen/home_screen.dart';
-import 'package:flutter_standard_app/screens/splash_screen/splash_screen.dart';
+import 'package:flutter_standard_app/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,82 +10,4 @@ void main() async {
       path: 'assets/translations',
       fallbackLocale: Locale('en', 'US'),
       child: MyApp()));
-}
-
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final navKey = GlobalKey<NavigatorState>();
-
-  Widget addReposProviders({required Widget child}) {
-    return MultiRepositoryProvider(providers: [
-      RepositoryProvider(create: (context) => AuthRepository(logOut: logOut)),
-      RepositoryProvider(
-          create: (context) => UserRepository(
-              logOut: logOut, authRepository: context.read<AuthRepository>()))
-    ], child: child);
-  }
-
-  Widget addBlocsProviders({required Widget child}) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (context) =>
-              AuthCubit(authRepository: context.read<AuthRepository>()),
-        ),
-      ],
-      child: child,
-    );
-  }
-
-  void logOut() {
-    _authCubit.logOut();
-  }
-
-  void listenAuthState(AuthState state) {
-    Future.delayed(Duration(milliseconds: 100), () {
-      if (state.state == AuthStateEnum.loggedIn) {
-        navKey.currentState!.pushAndRemoveUntil(
-            MaterialPageRoute(builder: (_) => HomeScreen()), (route) => false);
-        return;
-      }
-      if (state.state == AuthStateEnum.logOut) {
-        navKey.currentState!.pushAndRemoveUntil(
-            MaterialPageRoute(builder: (_) => SplashScreen()),
-            (route) => false);
-        return;
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return addReposProviders(
-      child: addBlocsProviders(
-        child: BlocBuilder<AuthCubit, AuthState>(builder: (_, authState) {
-          return BlocListener<AuthCubit, AuthState>(
-            listener: (_, state) {
-              listenAuthState(state);
-            },
-            listenWhen: (_, state) {
-              return state.state == AuthStateEnum.logOut ||
-                  state.state == AuthStateEnum.loggedIn;
-            },
-            child: MaterialApp(
-              navigatorKey: navKey,
-              localizationsDelegates: context.localizationDelegates,
-              supportedLocales: context.supportedLocales,
-              locale: context.locale,
-              home: SplashScreen(),
-            ),
-          );
-        }),
-      ),
-    );
-  }
-
-  AuthCubit get _authCubit => context.read<AuthCubit>();
 }
